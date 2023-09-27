73a5c790b49b56c4c542195c2dc6949d55b16d45
Mariusz94 <lyszczarz.mariusz@gmail.com>
2023-09-27 17:21:52 +0200

    change address and add timezone to docker

--- app/config.py
+++ app/config.py
@@ -35,13 +35,13 @@ LOGGER_FORMAT_DEBUG = "%(asctime)s.%(msecs)03d-%(levelname)s-%(filename)s.%(func
 LOGGER_FORMAT = LOGGER_FORMAT_INFO if LOGGING_MODE == "INFO" else LOGGER_FORMAT_DEBUG
 """Logger format."""
 
-BS_DB_CONNECTOR_MS_IP = str(os.environ.get("BS_DB_CONNECTOR_MS_IP", "localhost"))
+BS_DB_CONNECTOR_MS_IP = str(os.environ.get("BS_DB_CONNECTOR_MS_IP", "192.168.94.12"))
 """Address bs-db_connector-ms microservice"""
 
 BS_DB_CONNECTOR_MS_PORT = str(os.environ.get("BS_DB_CONNECTOR_MS_PORT", "91"))
 """Port bs-db_connector-ms microservice"""
 
-BS_DATABASE_IP = str(os.environ.get("BS_DATABASE_IP", "localhost"))
+BS_DATABASE_IP = str(os.environ.get("BS_DATABASE_IP", "192.168.94.12"))
 """Database IP."""
 
 BS_DATABASE_PORT = str(os.environ.get("BS_DATABASE_PORT", "6666"))
